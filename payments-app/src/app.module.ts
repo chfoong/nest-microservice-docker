f8baa6171fa879c0b81252a9480f820639b6065f
chfoong <chfoong@outlook.com>
2019-03-31 02:15:33 +0800

    Removed GET / hello world placeholder

--- payments-app/src/app.module.ts
+++ payments-app/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PaymentsModule } from './payments/payments.module';
 import { AuthService } from './common/auth/auth/auth.service';
 import { ConfigurationService } from './common/configuration/configuration/configuration.service';
@@ -8,7 +6,6 @@ import { AuthModule } from './common/auth/auth/auth.module';
 
 @Module({
   imports: [PaymentsModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService, AuthService, ConfigurationService],
+  providers: [AuthService, ConfigurationService],
 })
 export class AppModule {}
